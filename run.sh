11086ad1101779c37e66ecf3770cee571bc85e5d
JMHolmes-source <joshua.holmes@bbd.co.za>
2025-04-16 20:43:30 +0200

    test

---
+++ run.sh
@@ -0,0 +1,8 @@
+cd publish
+sudo pkill -f Galavisor
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+export ASPNETCORE_HTTP_PORTS=80
+chmod +X GalavisorApi
+sudo -E nohup ./GalavisorApi > /dev/null 2>&1 & exit
